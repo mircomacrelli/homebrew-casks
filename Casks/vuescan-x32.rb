bca13216416a05ebfacd7a70c2dac435ae4b82ee
Mirco Macrelli <mail@mircomacrelli.net>
2019-04-23 11:24:36 +0200

    add canon-lide-90 and vuescan-x32

---
+++ Casks/vuescan-x32.rb
@@ -0,0 +1,10 @@
+cask 'vuescan-x32' do
+  version '9.6.36'
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://www.hamrick.com/files/vuex32#{version.major_minor.no_dots}.dmg"
+  name 'VueScan'
+  homepage 'https://www.hamrick.com/'
+
+  app 'VueScan.app'
+end
