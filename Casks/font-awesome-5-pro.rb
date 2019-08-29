cb9353d86f66e671030c4fd0adad62a75ab6382a
Mirco Macrelli <mail@mircomacrelli.net>
2019-08-29 20:07:07 +0200

    update font-awesome-5-pro to 5.10.2

--- Casks/font-awesome-5-pro.rb
+++ Casks/font-awesome-5-pro.rb
@@ -1,11 +1,12 @@
 cask 'font-awesome-5-pro' do
-  version '5.10.0-11'
-  sha256 '7526970fa6bf8ccda007dfda48ef8469ef8a3525d44534f3bafd36ffcd18996d'
+  version '5.10.2'
+  sha256 'bd95bda83afdf96142d2d6d5e82372e35297ebb73cd6fda9dfbef0174ebec76a'
 
   url "https://#{File.read(File.expand_path('~/.cask-repository')).chomp}/Fonts/Font%20Awesome%205%20Pro/fontawesome-pro-#{version}-desktop.zip"
   name 'Font Awesome 5 Pro'
 
   font "fontawesome-pro-#{version}-desktop/otfs/Font Awesome 5 Brands-Regular-400.otf"
+  font "fontawesome-pro-#{version}-desktop/otfs/Font Awesome 5 Duotone-Solid-900.otf"
   font "fontawesome-pro-#{version}-desktop/otfs/Font Awesome 5 Pro-Light-300.otf"
   font "fontawesome-pro-#{version}-desktop/otfs/Font Awesome 5 Pro-Regular-400.otf"
   font "fontawesome-pro-#{version}-desktop/otfs/Font Awesome 5 Pro-Solid-900.otf"
