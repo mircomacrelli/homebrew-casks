572507ea7c71b9b9038e41e56c67984f937c17f4
Mirco Macrelli <mail@mircomacrelli.net>
2019-12-17 08:30:06 +0100

    update vuescan-x32 to 9.7.12

--- Casks/vuescan-x32.rb
+++ Casks/vuescan-x32.rb
@@ -1,5 +1,5 @@
 cask 'vuescan-x32' do
-  version '9.6.45'
+  version '9.7.12'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex32#{version.major_minor.no_dots}.dmg"
