bc50ee48983750876e820aa5992efba7b6344143
Mirco Macrelli <mail@mircomacrelli.net>
2020-07-19 13:10:09 +0200

    update pgmodeler to 0.9.3-beta

--- Casks/pgmodeler.rb
+++ Casks/pgmodeler.rb
@@ -1,6 +1,6 @@
 cask 'pgmodeler' do
-  version '0.9.3-alpha1'
-  sha256 '3a6af3efb634d76a582d7e16a9b2440135278ec9a6af63a958a2f2fd6ce0b2a5'
+  version '0.9.3-beta'
+  sha256 '091ed96e127ba35a453e55afc04dc9997ae52ff12f830ae74e1dcbf7e4d17bd0'
 
   url "https://#{File.read(File.expand_path('~/.cask-repository')).chomp}/Applications/macOS/pgModeler/pgmodeler-#{version}-macos.dmg"
   name 'pgModeler'
