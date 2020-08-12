2f397177c40b62312b185189fa38b1a24db14f55
Mirco Macrelli <mail@mircomacrelli.net>
2020-08-12 12:56:10 +0200

    upgrade font-awesome-pro to 5.14.0

--- Casks/font-awesome-5-pro.rb
+++ Casks/font-awesome-5-pro.rb
@@ -1,6 +1,6 @@
 cask 'font-awesome-5-pro' do
-  version '5.13.1'
-  sha256 '8da40a134627906cb7e57ab7c771fa681e6cbd1e65e5b49187d65d235295e8af'
+  version '5.14.0'
+  sha256 '812a48dc3f0a5d1d77329cb33968015620d81b47f35ab905a850ade0feffc406'
 
   url "https://#{File.read(File.expand_path('~/.cask-repository')).chomp}/Fonts/Font%20Awesome%205%20Pro/fontawesome-pro-#{version}-desktop.zip"
   name 'Font Awesome 5 Pro'
