f10f416ae32ee0497c8c9a91c493b3a9c5609a56
Mirco Macrelli <mail@mircomacrelli.net>
2021-01-04 19:59:55 +0100

    update hopper disassembler to 4.7.0

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,10 +1,10 @@
 cask 'hopper-disassembler' do
-  version '4.5.28'
-  sha256 '6fe10505613229f942d1906bd6ed93f61ef3479377bcdc7e25699b11e9d2f6b3'
+  version '4.7.0'
+  sha256 '502116ff36a0caa050f04a1f3d02e610ae8b6f8b4876a847a0dee79404d1d7ab'
 
   url "https://#{File.read(File.expand_path('~/.cask-repository')).chomp}/Applications/macOS/Hopper%20Disassembler/Hopper-#{version}.dmg"
   name 'Hopper Disassembler'
   homepage 'https://www.hopperapp.com/'
 
-  app 'Hopper Disassembler v4.app'
+  app 'Hopper Disassembler.app'
 end
