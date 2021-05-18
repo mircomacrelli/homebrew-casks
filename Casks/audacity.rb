250435de1c53aaaa08a49bfd3ebd90a653db0e3a
Mirco Macrelli <mail@mircomacrelli.net>
2021-05-18 18:55:48 +0200

    update audacity to version 3.0.2

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,6 +1,6 @@
 cask 'audacity' do
-  version '2.4.2'
-  sha256 '4730abe5b59d9c3dd000fde22d7037af6e6019a4305195a3e4e714f6c9f6380a'
+  version '3.0.2'
+  sha256 '57895aad20a5512aeb44d1ab5b7d01a68a5303d4225351528f377eb7686f281e'
 
   url "https://#{File.read(File.expand_path('~/.cask-repository')).chomp}/Applications/macOS/Audacity/audacity-macos-#{version}.dmg"
   name 'Audacity'
